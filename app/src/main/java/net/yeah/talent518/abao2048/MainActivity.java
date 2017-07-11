a9facba21fbf8540db4a297a9160084c4cae6498
ZhangBaoCai <talent518@yeah.net>
2017-07-11 17:31:18 +0800

    完成界面设计。

--- app/src/main/java/net/yeah/talent518/abao2048/MainActivity.java
+++ app/src/main/java/net/yeah/talent518/abao2048/MainActivity.java
@@ -2,8 +2,11 @@ package net.yeah.talent518.abao2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
